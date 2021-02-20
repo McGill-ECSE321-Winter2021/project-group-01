e1ae695574932e28dcbce5c8081b7f9e7fec80b9
ericchehata <70850294+ericchehata@users.noreply.github.com>
2021-02-20 15:30:45 +0200

    Setting up project

---
+++ AutoRepair-Backend/src/test/java/ca/mcgill/ecse321/autoRepair/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.autoRepair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

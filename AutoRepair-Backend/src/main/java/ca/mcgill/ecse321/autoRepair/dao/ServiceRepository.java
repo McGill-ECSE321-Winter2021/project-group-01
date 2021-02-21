6948557c7d9d2c905898b6afbb90f744c57c4238
faditawfik21 <fadi.tawfikbeshay@mail.mcgill.ca>
2021-02-21 23:46:11 +0200

    completed interface classes

--- AutoRepair-Backend/src/main/java/ca/mcgill/ecse321/autoRepair/dao/ServiceRepository.java
+++ AutoRepair-Backend/src/main/java/ca/mcgill/ecse321/autoRepair/dao/ServiceRepository.java
@@ -2,8 +2,9 @@ package ca.mcgill.ecse321.autoRepair.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
+
 import ca.mcgill.ecse321.autoRepair.model.Service;
 
 public interface ServiceRepository extends CrudRepository<Service, String>{
-
+	Service findServiceByName(String name);
 }

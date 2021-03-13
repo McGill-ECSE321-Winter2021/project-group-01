e27877ef2cec231f0a80aa44a6a124751345a790
mohammad-saeid-nafar <70984779+mohammad-saeid-nafar@users.noreply.github.com>
2021-03-13 10:15:06 -0500

    Done with ReviewDTO and ReminderDTO

--- AutoRepair-Backend/src/main/java/ca/mcgill/ecse321/autoRepair/dto/ReminderDto.java
+++ AutoRepair-Backend/src/main/java/ca/mcgill/ecse321/autoRepair/dto/ReminderDto.java
@@ -1,5 +1,30 @@
 package ca.mcgill.ecse321.autoRepair.dto;
 
+import java.sql.Date;
+import java.sql.Time;
+
 public class ReminderDTO {
+	
+	private String description;
+	private Date date;
+	private Time time;
+	
+	public ReminderDTO(String description, Date date, Time time) {
+		this.description = description;
+		this.date = date;
+		this.time = time;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public Time getTime() {
+		return time;
+	}
 
 }
--- AutoRepair-Backend/src/main/java/ca/mcgill/ecse321/autoRepair/dto/ReviewDto.java
+++ AutoRepair-Backend/src/main/java/ca/mcgill/ecse321/autoRepair/dto/ReviewDto.java
@@ -1,5 +1,21 @@
 package ca.mcgill.ecse321.autoRepair.dto;
 
 public class ReviewDTO {
+	
+	private String description;
+	private int serviceRating;
+	
+	public ReviewDTO(String description, int serviceRating) {
+		this.description = description;
+		this.serviceRating = serviceRating;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public int getServiceRating() {
+		return serviceRating;
+	}
 
 }

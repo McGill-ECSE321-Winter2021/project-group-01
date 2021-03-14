65b4247d0e90cdc7f0737df30869ab58febbd83c
RobertApra <robert.aprahamian@mail.mcgill.ca>
2021-03-14 11:15:38 +0200

    some changes

--- AutoRepair-Backend/src/main/java/ca/mcgill/ecse321/autoRepair/controller/ChosenServiceController.java
+++ AutoRepair-Backend/src/main/java/ca/mcgill/ecse321/autoRepair/controller/ChosenServiceController.java
@@ -1,5 +1,30 @@
 package ca.mcgill.ecse321.autoRepair.controller;
 
+import ca.mcgill.ecse321.autoRepair.dao.ChosenServiceRepository;
+import ca.mcgill.ecse321.autoRepair.dto.ChosenServiceDTO;
+import ca.mcgill.ecse321.autoRepair.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
 public class ChosenServiceController {
 
+	@Autowired
+	ChosenServiceRepository chosenServiceRepository;
+
+	private ChosenServiceDTO convertToDTO(ChosenService servicito) {
+		if(servicito==null) throw new IllegalArgumentException("Service not found.");
+		return new ChosenServiceDTO(servicito.getName(), servicito.getDuration());
+	}
+ 
 }
+
+
--- AutoRepair-Backend/src/main/java/ca/mcgill/ecse321/autoRepair/dto/ChosenServiceDTO.java
+++ AutoRepair-Backend/src/main/java/ca/mcgill/ecse321/autoRepair/dto/ChosenServiceDTO.java
@@ -1,4 +1,21 @@
 package ca.mcgill.ecse321.autoRepair.dto;
 
 public class ChosenServiceDTO {
+	
+	private String name;
+	private int duration;
+
+	public ChosenServiceDTO(String name,int duration) {
+		this.name = name;
+		this.duration = duration;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+	
 }

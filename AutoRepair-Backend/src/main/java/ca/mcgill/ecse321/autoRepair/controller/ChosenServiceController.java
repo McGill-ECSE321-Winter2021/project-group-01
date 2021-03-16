7ebd6b21ac840301f50cc26256a2330129687ce6
MarcSaber <marc.saber@mail.mcgill.ca>
2021-03-16 23:02:58 +0200

    Update ChosenServiceController.java

--- AutoRepair-Backend/src/main/java/ca/mcgill/ecse321/autoRepair/controller/ChosenServiceController.java
+++ AutoRepair-Backend/src/main/java/ca/mcgill/ecse321/autoRepair/controller/ChosenServiceController.java
@@ -9,7 +9,6 @@ import ca.mcgill.ecse321.autoRepair.service.ChosenServiceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.List;
 import java.util.stream.Collectors;
 

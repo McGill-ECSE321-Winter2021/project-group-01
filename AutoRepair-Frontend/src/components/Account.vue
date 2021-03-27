a72603009a3005ae2fbb7f7883ba141a977a94da
MarcSaber <marc.saber@mail.mcgill.ca>
2021-03-27 15:07:57 +0200

    commit to merge

--- AutoRepair-Frontend/src/components/Account.vue
+++ AutoRepair-Frontend/src/components/Account.vue
@@ -10,7 +10,7 @@
       <br>
       <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
     </ul>
-    <h2>Ecosystem</h2>
+    <h2>AutoRepair Application</h2>
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
@@ -20,33 +20,6 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to ecosystem'
-    }
-  }
-}
+<script src="./Account.js">
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
- li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>

1cf48b1b6f0eec721d11e3597f12ce707b68df98
ericchehata <70850294+ericchehata@users.noreply.github.com>
2021-04-13 02:22:57 +0200

    Tried to link to backend

--- AutoRepair-Android/app/src/main/java/ca/mcgill/ecse321/autoRepair/MainActivity.java
+++ AutoRepair-Android/app/src/main/java/ca/mcgill/ecse321/autoRepair/MainActivity.java
@@ -4,16 +4,24 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    private String error = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +29,99 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+    private void refreshErrorMessage() {
+        // set the error message
+        TextView tvError = (TextView) findViewById(R.id.error);
+        tvError.setText(error);
+
+        if (error == null || error.length() == 0) {
+            tvError.setVisibility(View.GONE);
+        } else {
+            tvError.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+
+    public void editProfile(View view){
+        error ="";
+
+        TextView tv = (TextView) findViewById(R.id.firstName);
+        String firstName = tv.getText().toString();
+
+        tv = (TextView) findViewById(R.id.lastName);
+        String lastName = tv.getText().toString();
+
+        tv = (TextView) findViewById(R.id.address);
+        String address = tv.getText().toString();
+
+        tv = (TextView) findViewById(R.id.phoneNumber);
+        String phoneNumber = tv.getText().toString();
+
+        tv = (TextView) findViewById(R.id.zipCode);
+        String zipCode = tv.getText().toString();
+
+        tv = (TextView) findViewById(R.id.email);
+        String email = tv.getText().toString();
+
+        RequestParams rp = new RequestParams();
+        rp.add("firstName", firstName);
+        rp.add("lastName", lastName);
+        rp.add("phoneNumber", phoneNumber);
+        rp.add("email", email);
+        rp.add("address", address);
+        rp.add("zipCode", zipCode);
+
+
+        HttpUtils.patch("edit_profile/" + "bob", rp, new JsonHttpResponseHandler() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
+                try {
+                    JSONObject serverResp = new JSONObject(response.toString());
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                // refreshErrorMessage();
+                //  ((TextView) v.findViewById(R.id.newevent_name)).setText("");
+            }
+
+            @Override
+            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
+            }
+        });
+    }
+
+    public void getProfile(View v) {
+        error = "";
+        HttpUtils.get("profiles", new RequestParams(), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
+                try {
+                    JSONObject serverResp = new JSONObject(response.toString());
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                // refreshErrorMessage();
+                //  ((TextView) v.findViewById(R.id.newevent_name)).setText("");
+            }
+
+            @Override
+            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
             }
         });
     }
@@ -53,4 +147,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }

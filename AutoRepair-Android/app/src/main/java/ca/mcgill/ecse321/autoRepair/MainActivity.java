004608d49be189935eb9ac12605154d44c9f1c9e
ericchehata <70850294+ericchehata@users.noreply.github.com>
2021-04-13 17:27:32 +0200

    Profile stuff

--- AutoRepair-Android/app/src/main/java/ca/mcgill/ecse321/autoRepair/MainActivity.java
+++ AutoRepair-Android/app/src/main/java/ca/mcgill/ecse321/autoRepair/MainActivity.java
@@ -2,23 +2,32 @@ package ca.mcgill.ecse321.autoRepair;
 
 import android.os.Bundle;
 
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
-
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private String error = null;
@@ -49,37 +58,45 @@ public class MainActivity extends AppCompatActivity {
     public void editProfile(View view){
         error ="";
 
-        TextView tv = (TextView) findViewById(R.id.firstName);
-        String firstName = tv.getText().toString();
+//        TextView tv = (EditText) findViewById(R.id.firstName);
+//        String firstName = tv.getText().toString();
+//
+//        tv = (EditText) findViewById(R.id.lastName);
+//        String lastName = tv.getText().toString();
+//
+//        tv = (EditText) findViewById(R.id.address);
+//        String address = tv.getText().toString();
+//
+//        tv = (EditText) findViewById(R.id.phoneNumber);
+//        String phoneNumber = tv.getText().toString();
+//
+//        tv = (EditText) findViewById(R.id.zipCode);
+//        String zipCode = tv.getText().toString();
+//
+//        tv = (EditText) findViewById(R.id.email);
+//        String email = tv.getText().toString();
 
-        tv = (TextView) findViewById(R.id.lastName);
-        String lastName = tv.getText().toString();
-
-        tv = (TextView) findViewById(R.id.address);
-        String address = tv.getText().toString();
-
-        tv = (TextView) findViewById(R.id.phoneNumber);
-        String phoneNumber = tv.getText().toString();
-
-        tv = (TextView) findViewById(R.id.zipCode);
-        String zipCode = tv.getText().toString();
-
-        tv = (TextView) findViewById(R.id.email);
-        String email = tv.getText().toString();
+        final EditText firstName = (EditText) findViewById(R.id.firstName);
+        final EditText lastName = (EditText) findViewById(R.id.lastName);
+        final EditText address = (EditText) findViewById(R.id.address);
+        final EditText phoneNumber = (EditText) findViewById(R.id.phoneNumber);
+        final EditText zipCode = (EditText) findViewById(R.id.zipCode);
+        final EditText email = (EditText) findViewById(R.id.email);
 
         RequestParams rp = new RequestParams();
-        rp.add("firstName", firstName);
-        rp.add("lastName", lastName);
-        rp.add("phoneNumber", phoneNumber);
-        rp.add("email", email);
-        rp.add("address", address);
-        rp.add("zipCode", zipCode);
+        rp.put("firstName", firstName.getText());
+        rp.put("lastName", lastName.getText());
+        rp.put("phoneNumber", phoneNumber.getText());
+        rp.put("email", email.getText());
+        rp.put("address", address.getText());
+        rp.put("zipCode", zipCode.getText());
 
 
         HttpUtils.patch("edit_profile/" + "bob", rp, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
                 try {
+                    //JSONObject server = response.getJSONObject(0);
                     JSONObject serverResp = new JSONObject(response.toString());
                 } catch (JSONException e) {
                     error += e.getMessage();
@@ -103,6 +120,42 @@ public class MainActivity extends AppCompatActivity {
     public void getProfile(View v) {
         error = "";
         HttpUtils.get("profiles", new RequestParams(), new JsonHttpResponseHandler() {
+            ArrayList<JSONObject> profiles = new ArrayList<JSONObject>();
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONArray response) {
+
+                try {
+                        for (int i=0; i<response.length(); i++){
+                            profiles.add(response.getJSONObject(i));
+                        }
+
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                // refreshErrorMessage();
+                //  ((TextView) v.findViewById(R.id.newevent_name)).setText("");
+            }
+
+            @Override
+            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+                refreshErrorMessage();
+            }
+        });
+    }
+
+    public void login(View v){
+        String username = "grader";
+        String password = "Password1!";
+        RequestParams rp = new RequestParams();
+        rp.add("username", username);
+        rp.add("password", password);
+
+        HttpUtils.post("login", rp, new JsonHttpResponseHandler(){
             @Override
             public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
                 try {
@@ -124,7 +177,9 @@ public class MainActivity extends AppCompatActivity {
                 refreshErrorMessage();
             }
         });
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- AutoRepair-Android/app/src/main/java/ca/mcgill/ecse321/autoRepair/Profile.java
+++ AutoRepair-Android/app/src/main/java/ca/mcgill/ecse321/autoRepair/Profile.java
@@ -7,9 +7,8 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
-public class Signup extends Fragment {
+public class Profile extends Fragment {
 
     @Override
     public View onCreateView(
@@ -17,7 +16,7 @@ public class Signup extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.signup, container, false);
+        return inflater.inflate(R.layout.profile, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {

9ab93d0fcd146e991c78c280deca5be7a4ed697b
RobertApra <robert.aprahamian@mail.mcgill.ca>
2021-04-15 01:30:30 +0200

    service done

--- AutoRepair-Android/app/src/main/java/ca/mcgill/ecse321/autoRepair/Home.java
+++ AutoRepair-Android/app/src/main/java/ca/mcgill/ecse321/autoRepair/Home.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -21,9 +23,9 @@ public class Home extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
 
-
-
-        super.onViewCreated(view, savedInstanceState);
+        ((MainActivity)getActivity()).getServices(view);
+        //((MainActivity)getActivity()).getReminders(view);
+        //super.onViewCreated(view, savedInstanceState);
 
     }
 }

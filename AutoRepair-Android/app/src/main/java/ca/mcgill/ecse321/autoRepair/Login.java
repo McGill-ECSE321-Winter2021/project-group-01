712e4885c4f8d6512a28d81a81273685bd878c02
MarcSaber <marc.saber@mail.mcgill.ca>
2021-04-14 21:52:51 +0300

    commit to merge

--- AutoRepair-Android/app/src/main/java/ca/mcgill/ecse321/autoRepair/Login.java
+++ AutoRepair-Android/app/src/main/java/ca/mcgill/ecse321/autoRepair/Login.java
@@ -1,25 +1,71 @@
 package ca.mcgill.ecse321.autoRepair;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
 public class Login extends Fragment {
+    private String error = "";
 
-    @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.login, container, false);
+
+    public void login(View androidV) {
+        RequestParams requestParams = new RequestParams();
+        String username = "grader";
+        String password = "Password1!";
+        requestParams.add("username", username);
+        requestParams.add("password", password);
+        HttpUtils.post("login", requestParams, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONObject serverResp = new JSONObject(response.toString());
+                } catch (JSONException e) {
+                    error +="error";
+                }
+//                refreshErrorMessage();
+//                tv.setText("");
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error +=e.getMessage();
+                }
+//                refreshErrorMessage();
+            }
+        });
     }
-
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
@@ -31,4 +77,13 @@ public class Login extends Fragment {
             }
         });
     }
+
 }
+
+
+
+
+
+
+
+
--- AutoRepair-Android/app/src/main/java/ca/mcgill/ecse321/autoRepair/Signup.java
+++ AutoRepair-Android/app/src/main/java/ca/mcgill/ecse321/autoRepair/Signup.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse321.autoRepair;
 
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,18 +10,31 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
 public class Signup extends Fragment {
+    private String error = "";
 
-    @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.signup, container, false);
+        @Override
+        public View onCreateView(
+                LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState
+        ) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.signup, container, false);
+        }
+
+        public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+        }
+
+
+
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    }
-}

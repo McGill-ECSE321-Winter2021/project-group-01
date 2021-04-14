4c5a96142d68ca07fbf4a565c973d29ca156b689
RobertApra <robert.aprahamian@mail.mcgill.ca>
2021-04-14 22:18:34 +0200

    start

--- AutoRepair-Android/app/src/main/java/ca/mcgill/ecse321/autoRepair/Home.java
+++ AutoRepair-Android/app/src/main/java/ca/mcgill/ecse321/autoRepair/Home.java
@@ -20,6 +20,9 @@ public class Home extends Fragment {
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+
+
+
         super.onViewCreated(view, savedInstanceState);
 
     }

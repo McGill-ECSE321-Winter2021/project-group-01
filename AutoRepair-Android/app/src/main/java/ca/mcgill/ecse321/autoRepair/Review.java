cc71c547582d9effe11c62d8d12b381cfc6fa479
mohammad-saeid-nafar <70984779+mohammad-saeid-nafar@users.noreply.github.com>
2021-04-15 14:31:40 -0400

    Comitting

--- AutoRepair-Android/app/src/main/java/ca/mcgill/ecse321/autoRepair/Review.java
+++ AutoRepair-Android/app/src/main/java/ca/mcgill/ecse321/autoRepair/Review.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-public class Review extends Fragment {
+public class Review extends Fragment implements AdapterView.OnItemSelectedListener {
 
     @Override
     public View onCreateView(
@@ -21,6 +25,24 @@ public class Review extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        Spinner ratingSpinner = view.findViewById(R.id.rating);
+
+        ArrayAdapter<CharSequence> ratingAdapter = ArrayAdapter.createFromResource(this.getContext(),
+                R.array.serviceRating, android.R.layout.simple_spinner_item);
+        ratingAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ratingSpinner.setAdapter(ratingAdapter);
+        ratingSpinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
